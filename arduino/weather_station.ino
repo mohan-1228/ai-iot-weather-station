eda390c400cc0a223091e00b2052d491d6a0d0fd
Mohan Thapa <itsmohanthapa1999@gmail.com>
2026-06-13 19:39:37 -0500

    Add initial DHT11 weather station code

---
+++ arduino/weather_station.ino
@@ -0,0 +1,28 @@
+#include "DHT.h"
+
+#define DHTPIN 2
+#define DHTTYPE DHT11
+
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup(){
+Serial.begin(9600);
+dht.begin();
+}
+
+void loop(){
+float humidity = dht.readHumidity();
+float temperature = dht.readTemperature();
+
+if(isnan(humidity) || isnan(temperature)){
+Serial.ptintln("Error");
+delay(2000);
+return;
+}
+
+Serial.print(temperature);
+Serial.print(",");
+Serial.println(humidity);
+
+delay(2000)
+}
